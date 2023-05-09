e0ea2286fc2fd760572ab2f40cdee9384d638dd8
juyub <jjuyub@gmail.com>
2023-05-09 13:07:42 +0900

    js 기본구조, if제어문

---
+++ 0509_js/utils.js
@@ -0,0 +1,17 @@
+// return X, parameter X
+function hi() {
+  //   alert("Hi!!!");
+
+  console.log("Hi!!!!!!!");
+
+  //   console.log();   log + enter로도 가능
+}
+
+function sum(x, y) {
+  let result = x + y;
+  console.log(result);
+}
+
+function total(x, y) {
+  return x + y;
+}
